d17775f1f9b9ee7e69e90b4fb751aafd43b062a3
FAHADALI2001 <m.mohsin00933@gmail.com>
2023-10-23 06:11:49 -0700

    fahad

--- lib/src/views/favorite/favorite_screen.dart
+++ lib/src/views/favorite/favorite_screen.dart
@@ -1,4 +1,7 @@
+import 'package:e_commerce/src/const/paddings/paddings.dart';
+import 'package:e_commerce/src/views/drawer/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
@@ -10,6 +13,96 @@ class FavoriteScreen extends StatefulWidget {
 class _FavoriteScreenState extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    Size size = MediaQuery.sizeOf(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: GestureDetector(
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  PageTransition(
+                      type: PageTransitionType.leftToRightWithFade,
+                      duration: const Duration(seconds: 1),
+                      child: MainDrawer()));
+            },
+            child: const Icon(
+              Icons.arrow_back,
+              color: Colors.grey,
+            )),
+      ),
+      body: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: horizontal, vertical: vertical),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Cart",
+              style: TextStyle(
+                  color: Colors.grey.shade700,
+                  fontWeight: FontWeight.bold,
+                  fontSize: size.width * 0.07),
+            ),
+            //
+            SizedBox(
+              height: size.height * 0.02,
+            ),
+            //
+            Expanded(
+              child: GridView.builder(
+                itemCount: 5,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisExtent: size.height * 0.3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10),
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: InkWell(
+                      onTap: () {
+                        // Navigator.pushReplacement(
+                        //     context,
+                        //     PageTransition(
+                        //         type: PageTransitionType.rightToLeftWithFade,
+                        //         duration: const Duration(seconds: 1),
+                        //         child: const SingleProductScreen()));
+                      },
+                      child: Card(
+                        elevation: 10,
+                        child: Container(
+                          width: size.width * 0.3,
+                          color: Colors.white,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Image.asset(
+                                  "assets/images/shirt.jpg",
+                                  width: size.width,
+                                  height: size.height * 0.2,
+                                  fit: BoxFit.cover,
+                                ),
+                                //
+
+                                const Text("\$55.00"),
+                                const Text("Men T-Shirt"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
