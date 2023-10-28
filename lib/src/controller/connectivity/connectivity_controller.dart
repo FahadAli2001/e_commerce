91ae00b9b14ff4acc3ab608747fca5eb497eceec
FAHADALI2001 <m.mohsin00933@gmail.com>
2023-10-28 05:54:59 -0700

    fahad

--- lib/src/controller/connectivity/connectivity_controller.dart
+++ lib/src/controller/connectivity/connectivity_controller.dart
@@ -1,13 +1,8 @@
 import 'dart:async';
 import 'dart:developer';
-
-import 'package:e_commerce/src/commans/custom_error_snackbar/custom_error_snackbar.dart';
-
-import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:connectivity_plus/connectivity_plus.dart'; 
 import 'package:flutter/material.dart';
-import 'package:top_snackbar_flutter/custom_snack_bar.dart';
-import 'package:top_snackbar_flutter/top_snack_bar.dart';
-
+  
 final Connectivity connectivity = Connectivity();
 late StreamSubscription<ConnectivityResult> subscription;
 
